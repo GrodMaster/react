877b362f1db666d82519117ab26e0a9e67ccf45c
Виктор Иванов <v.ivaniv@agrohold.ru>
2023-07-03 11:00:05 +0300

    add loading

---
+++ app/pogoda/loading.tsx
@@ -0,0 +1,7 @@
+import styleForm from '../style/modules/Pogoda.module.scss'
+
+
+export default function Loading() {
+    return <div className={styleForm.item}>
+    <h3>Loading...</h3>
+    </div>}
