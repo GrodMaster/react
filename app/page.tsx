d3fdaa30943dfcb4cafc079a55b2a2469b508722
GrodMaster <grodmaster2517@gmail.com>
2023-07-03 23:41:46 +0300

    test mail

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-
+import './style/form.scss'
 // import  style  from './style/modules/Home.module.scss'
 export default function Home() {
   return (
-   <div className="rows">
- 
-   </div>
+<div className='row'>
+  <div className='form'>
+<form action='./public.send.php' method='POST'>
+<input type="text" name="name" placeholder="Имя"/>  
+<input type="text" name="email" placeholder="Email"/>  
+<button>Отправить</button>
+</form>
+</div>
+<div className='main'></div>
+</div>
   )
 }
